2938fb3f8b49f862b93dde11d8472572bff466f8
PIBO0121038 Фомин Иван Константинович <PIBO0121038@sibupk.su>
2023-01-14 06:42:42 +0700

    Добавьте файлы проекта.

---
+++ MathTaskClassLibraryTests/GeometryTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MathTaskClassLibrary;
+
+namespace MathTaskClassLibraryTests
+{
+    [TestClass]
+    public class GeometryTests
+    {
+        [TestMethod]
+        public void CalculateAreaTest()
+        {
+            //исходные данные
+            int a = 3;
+            int b = 5;
+            int expeced = 15; //ожидаемое значение
+
+        }
+        [TestMethod]
+        public void CalculateAreaInvalidDataTest1()
+        {
+            bool catched = false; //исключение было поймано
+            try
+            {
+                //исходные данные
+                int a = -4;
+                int b = 10;
+
+                Geometry g = new Geometry();
+                g.CalculateArea(a, b);
+            }
+            catch (ArgumentException e)
+            {
+                catched = true;
+            }
+            Assert.IsTrue(catched, "не обработаны допустимые данные");
+        }
+        [TestMethod]
+        public void CalculateAreaInvalidDataTest2()
+        {
+            //исходные данные
+            int a = -4;
+            int b = 10;
+            //int expecred = 15; //ожидаемое значение
+
+            //полученные значения с помощью тестируемого метода:
+            Geometry g = new Geometry();
+            int actual = g.CalculateArea(a, b);
+            //сравнение ожидаемого результата с полученными:
+            //Assert.AreEqual(expecred, actual);
+            Assert.ThrowsException<ArgumentException>(() => g.CalculateArea(a, b),
+                "не обработаны отрицательные длины сторон прямоугольника");
+        }
+    }
+}
---
+++ MathTaskClassLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MathTaskClassLibraryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MathTaskClassLibraryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b5da65f0-2a74-4916-ab89-75a4437acbbe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
